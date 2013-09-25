433f9061464a1edff6c67bf0e5e983f207bb43d3
Peter <peter.budo@gmail.com>
2013-09-25 17:59:33 +0100

    Simple constructor test

--- chap03/src/test/java/com/practicalunittesting/MoneyTest.java
+++ chap03/src/test/java/com/practicalunittesting/MoneyTest.java
@@ -1,5 +1,16 @@
 package com.practicalunittesting;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class MoneyTest {
 
+    @Test
+    public void constructorShouldSetAmountAndCurrency() {
+        Money money = new Money(10, "USD");
+
+        assertEquals(10, money.getAmount());
+        assertEquals("USD", money.getCurrency());
+    }
 }
