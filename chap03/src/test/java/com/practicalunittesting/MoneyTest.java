f92844a1165abae0b8b1aa56ea193d22bb500381
Peter <peter.budo@gmail.com>
2013-09-25 18:15:42 +0100

    Parameterized test

--- chap03/src/test/java/com/practicalunittesting/MoneyTest.java
+++ chap03/src/test/java/com/practicalunittesting/MoneyTest.java
@@ -1,16 +1,30 @@
 package com.practicalunittesting;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import static junit.framework.Assert.assertEquals;
+import static junitparams.JUnitParamsRunner.$;
 
+@RunWith(JUnitParamsRunner.class)
 public class MoneyTest {
 
-    @Test
-    public void constructorShouldSetAmountAndCurrency() {
-        Money money = new Money(10, "USD");
+    private static final Object[] getMoney() {
+        return $(
+                $(10, "USD"),
+                $(20, "EUR")
+        );
+    }
 
-        assertEquals(10, money.getAmount());
-        assertEquals("USD", money.getCurrency());
+    @Test
+    @Parameters(method = "getMoney")
+    public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
+        Money money = new Money(amount, currency);
+
+        assertEquals(amount, money.getAmount());
+        assertEquals(currency, money.getCurrency());
     }
 }
