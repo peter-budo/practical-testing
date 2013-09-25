9cbe186444e61ef5da62dd42256f3238319256b1
Peter <peter.budo@gmail.com>
2013-09-25 17:53:13 +0100

    Class to test

--- chap03/src/main/java/com/practicalunittesting/Money.java
+++ chap03/src/main/java/com/practicalunittesting/Money.java
@@ -1,5 +1,28 @@
 package com.practicalunittesting;
 
 public class Money {
+    private final int amount;
+    private final String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public boolean equals(Object anObject) {
+        if (anObject instanceof Money) {
+            Money money = (Money) anObject;
+            return  money.getClass().equals(getCurrency())
+                    && getAmount() == money.getAmount();
+        }
+        return false;
+    }
 }
